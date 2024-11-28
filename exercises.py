b51edec077023f63e8cb077387eb1463fe9b67fd
useradmin <amutawa@hotmail.com>
2024-11-28 10:22:20 +0300

    end

--- exercises.py
+++ exercises.py
@@ -8,3 +8,50 @@ def manage_students():
 print('Exercise 1:', manage_students())
 
 
+# Exercise 2: Loop and String Concatenation
+def combine_foods():
+    foods = ("Pizza", "Burger", "Pasta")
+    meal = ""
+    for food in foods:
+        meal += food + " "
+    return meal.strip()
+
+print('Exercise 2:', combine_foods())
+
+# Exercise 3: Slicing Tuples
+def slice_foods():
+    foods = ("Pizza", "Burger", "Pasta")
+    last_two_foods = foods[-2:]
+    return last_two_foods
+
+print('Exercise 3:', slice_foods())
+
+
+# Exercise 4: Dictionaries and String Formatting
+def hometown_info():
+    home_town = {
+        "city": "San Francisco",
+        "state": "California",
+        "population": 870000
+    }
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
+
+print('Exercise 4:', hometown_info())
+
+
+# Exercise 5: Iterating Over Dictionary Items
+def list_home_town_items():
+    home_town = {
+        "city": "San Francisco",
+        "state": "California",
+        "population": 870000
+    }
+    home_town_items = []
+    for key, value in home_town.items():
+        home_town_items.append(f"{key} = {value}")
+    return home_town_items
+
+print('Exercise 5:', list_home_town_items())
+
+
