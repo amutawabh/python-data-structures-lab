602a434355f1506f79cb24a1734f0fad20559bd1
useradmin <amutawa@hotmail.com>
2024-11-28 10:03:00 +0300

    exercise 1

---
+++ exercises.py
@@ -0,0 +1,10 @@
+# Exercise 1: List and Indexing
+def manage_students():
+    students = ["Alice", "Bob", "Charlie"]
+    first_student = students[1]
+    last_student = students[-1]
+    return first_student, last_student
+
+print('Exercise 1:', manage_students())
+
+
